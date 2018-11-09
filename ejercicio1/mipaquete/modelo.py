02be7b21866b6709ce3c83d102ad476f5d003b24
DAVID_BURNEO <davichov2013@gmail.com>
2018-11-09 12:53:23 -0500

    Segundo Git, Clase Empleado Fijo y por Horas en Python

--- ejercicio1/mipaquete/modelo.py
+++ ejercicio1/mipaquete/modelo.py
@@ -4,7 +4,7 @@ class Empleado(object):
         self.apellido = ""
         self.cedula = 0
         self.comision_fija = 0
-
+#Metodos agragar y obtener para nombre, apellido, cedula y comision
     def agregar_nombre(self, n):
         self.nombre = n
 
@@ -28,6 +28,91 @@ class Empleado(object):
 
     def obtener_comision_fija(self):
         return self.comision_fija
-
+#Se presentan los datos principales
     def presentar_datos(self):
         return "%s-%s-%s-%s" % (self.obtener_nombre(), self.apellido, self.cedula, self.comision_fija)
+#Clase para realizar el calculo del empleado por horas
+class EmpleadoPorHoras(Empleado):
+    def __init__(self):
+        super(EmpleadoPorHoras, self).__init__() 
+        #Nuevos atributos
+        self.numero_horas = 0
+        self.valor_hora = 0
+    #Metodos agregar y obtener    
+    def agregar_numero_horas(self, nh):
+        self.numero_horas = nh
+
+    def obtener_numero_horas(self):
+        return self.numero_horas
+
+    def agregar_valor_hora(self, vh):
+        self.valor_hora = vh
+
+    def obtener_valor_hora(self):
+        return self.valor_hora
+    #Se realiza el calculo del sueldo final
+    def calcular_valor_sueldo_final(self):
+        
+        sueldo_final = (self.obtener_numero_horas() * self.obtener_valor_hora()) + (self.obtener_comision_fija()) #Primero realizo la multiplicacipon del numero de horas por el precio de cada hora y luego le aumento el valor de la comision fija
+        return sueldo_final #Enviar el valor
+    #Presento los datos resultantes
+    def presentar_datos(self):
+        cadena = "%s \n numero_horas: %s \n valor_hora: %s \n sueldo_final: %s" % (super(EmpleadoPorHoras, self).presentar_datos(), self.obtener_numero_horas(), self.obtener_valor_hora(), self.calcular_valor_sueldo_final())
+        return cadena
+#Clase para empleado fijo
+class EmpleadoFijo(Empleado):
+    def __init__(self):
+        super(EmpleadoFijo, self).__init__()
+        #Nuevos atributos
+        self.sueldo_fijo = 0
+        self.descuento_seguro = 0
+    #Metodos agregar y obtener para los nuevos atributos
+    def agregar_sueldo_fijo(self, sf):
+        self.sueldo_fijo = sf
+
+    def obtener_sueldo_fijo(self):
+        return self.sueldo_fijo
+
+    def agregar_descuento_seguro(self, ds):
+        self.descuento_seguro = ds
+
+    def obtener_descuento_seguro(self):
+        return self.descuento_seguro
+
+   #Calculo del sueldo final
+    def calcular_valor_sueldo_final(self):
+        sueldo_final = (self.obtener_sueldo_fijo() + self.obtener_comision_fija()) - (self.obtener_sueldo_fijo() * (self.obtener_descuento_seguro() / 100)) #Primero sumo el sueldo inicial y la commision fija, y luego le resto el resultado de la multiplicacion de sueldoo fijo y descuento dividido para 100 para restar el porcentaje
+        return sueldo_final
+    #Presento los datos resultantes
+    def presentar_datos(self):
+        cadena = "%s\n sueldo_fijo: %s \n descuento_seguro: %s \n Sueldo_final: %s" % (super(EmpleadoFijo, self).presentar_datos(), self.obtener_sueldo_fijo(), self.obtener_descuento_seguro(), self.calcular_valor_sueldo_final())
+        return cadena
+
+class EmpleadoPorSemana(Empleado):
+    def __init__(self):
+        super(EmpleadoPorSemana, self).__init__()
+        #Nuevos atributos
+        self.numero_Semanas = 0
+        self.valor_semanal = 0
+
+    def agregar_numero_semanas(self, ns): 
+        self.numero_Semanas = ns
+
+    def obtener_numero_semanas(self):
+        return self.numero_Semanas
+
+    def agregar_valor_semanal(self, vs):
+        self.valor_semanal = vs
+
+    def obtener_valor_semanal(self):
+        return self.valor_semanal
+        #Calculo del sueldo final
+    def calcular_valor_sueldo_final(self):
+        sueldo_final = self.numero_Semanas() * self.valor_semanal() #Multiplicar el numero de semanas por el valor semanal
+        #Oresentar datos
+    def presentar_datos(self):
+        cadena = "%s ln numero_Semanas: %s ln valor_semanal: %s ln Sueldo_final: %s" % (super(EmpleadoPorSemana,self).presentar_datos(), self.obtener_numero_semanas(), self.obtener_valor_semanal(), self.calcular_valor_sueldo_final())
+        return cadena
+
+
+
--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -4,4 +4,32 @@ e = Empleado()
 e.agregar_nombre("Luis")
 e.agregar_apellido("Benitez")
 e.agregar_cedula("1110001")
+
 print(e.presentar_datos())
+
+e1 = EmpleadoPorHoras()
+nombre = input ("Ingrese su nombre ")
+e1.agregar_nombre(nombre)
+e1.agregar_apellido("andrade")
+e1.agregar_cedula("112235")
+e1.agregar_comision_fija(50)
+e1.agregar_valor_hora(20.2)
+e1.agregar_numero_horas(15)
+print(e1.presentar_datos())
+
+e2 = EmpleadoFijo()
+e2.agregar_sueldo_fijo(300.2)
+e2.agregar_descuento_seguro(10) #Porcentaje
+comision = input("ingrese comision \n")
+comision = float(comision) #Conversion de dato tipo cadena a float
+e2.agregar_comision_fija(comision)
+e2.nombre = "Ana"
+e2.apellido = "Diaz"
+print(e2.presentar_datos())
+
+#e3 = EmpleadoPorSemana()
+#e3.agregar_numero_semanas(5)
+#e3.agregar_valor_semanal(40)
+#e3.nombre = "Juan"
+#e3.apellido = "Perez"
+#print(e3.presentar_datos())
